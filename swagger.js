b0ee706b2875abbf8c8e80b1e51308a4b51a5ef3
spencer-rafada <spencer.byui@gmail.com>
2023-02-04 10:14:13 -0700

    fixed documentation

--- swagger.js
+++ swagger.js
@@ -3,10 +3,10 @@ const swaggerAutogen = require(`swagger-autogen`)();
 const doc = {
   info: {
     title: `REST API about Food`,
-    description: `This web service provides knowledge about different food all over the world.`,
-    host: `f00d-rest.onrender.com`,
-    schemes: [`https`]
-  }
+    description: `This web service provides knowledge about different food all over the world.`
+  },
+  host: `f00d-rest.onrender.com`,
+  schemes: [`https`]
 };
 
 const outputFile = `./swagger.json`;
