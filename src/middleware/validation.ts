576591a2d3e6fe79c0cbcbf03aa0ef674d46b6a8
spencer-rafada <spencer.byui@gmail.com>
2023-02-11 15:22:05 -0700

    updated swagger.json

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -15,7 +15,10 @@ const updateFoodValidation = [
   check(`foodName`, `Food Name cannot be set to empty`).not().isEmpty(),
   check('origin', 'Food origin cannot be set to empty.').not().isEmpty(),
   check(`cookTime`, `Cook Time cannot be less than 0`).notEmpty().isInt({ min: 0, max: 1440 }),
-  check(`category`, `Category cannot be empty and correct category.`).notEmpty().isIn(foodCategory)
+  check(`category`, `Category cannot be empty and correct category.`)
+    .toLowerCase()
+    .notEmpty()
+    .isIn(foodCategory)
 ];
 
 // Check if the string ID is a valid MongoDB Object Id
