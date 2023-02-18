d8d0bdfcb0b6d82313723927d2b588f133e04879
spencer-rafada <spencer.byui@gmail.com>
2023-02-18 15:07:13 -0700

    fixed authentication

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -4,7 +4,7 @@ dotenv.config();
 export default {
   authRequired: false,
   auth0Logout: true,
-  baseURL: `https://https://f00d-rest.onrender.com`,
+  baseURL: `https://f00d-rest.onrender.com`,
   clientID: process.env.CLIENT_ID,
   issuerBaseURL: process.env.ISSUER_BASE_URL,
   secret: process.env.SECRET
